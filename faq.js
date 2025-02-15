49fc2fd108d6a455fd788de434a63e42ecd28ca2
BAJENTING <bajentingjohn@gmail.com>
2025-02-16 02:09:27 +0800

    Updated css and js files

--- faq.js
+++ faq.js
@@ -20,3 +20,15 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('faqContent').style.display = 'none';
     document.getElementById('feedbackContent').style.display = 'none';
 });
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+  }
